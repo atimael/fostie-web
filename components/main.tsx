0c1e9f367c75422f8b5af7d695b45868c9d2cc0a
atimael <andreymileta@gmail.com>
2025-06-22 19:42:51 +0200

    fix: responsive fixes

--- components/main.tsx
+++ components/main.tsx
@@ -7,6 +7,7 @@ import { TimerContent } from "./landing/timer-content";
 import MediaPlayer from "./media-player";
 import { AppleIcon } from "./landing/apple-icon";
 import { KeyFeatures } from "./landing/key-features";
+import AppstoreButton from "./landing/appstore-button";
 
 export default function Main() {
   const [timerShow, setTimerShow] = useState(true);
@@ -51,9 +52,11 @@ export default function Main() {
         <h1 className="motion-preset-blur-down motion-delay-500 mx-auto max-w-3xl text-7xl leading-18 font-bold tracking-tight text-orange-50">
           Your focus companion for deep work
         </h1>
+
+        <AppstoreButton className={"motion-delay-600 flex pt-8 sm:hidden"} />
       </section>
 
-      <section className="motion-preset-blur-up motion-delay-1000 xs:mb-64 mb-96 w-full flex-col items-center sm:mb-64 sm:flex md:mb-72 lg:mb-24">
+      <section className="motion-preset-blur-up motion-delay-1000 mb-64 w-full flex-col items-center sm:mb-64 sm:flex md:mb-72 lg:mb-24">
         <div className="relative aspect-[1.95/1] h-full w-full">
           <div
             className="absolute top-8 right-8 left-8 z-10 flex h-12 items-center justify-between rounded-[1.25rem] bg-orange-50/5 pr-5 pl-3 text-orange-50 backdrop-blur-xl md:right-24 md:left-24"
@@ -83,7 +86,7 @@ export default function Main() {
 
           {timerShow && (
             <TimerContent
-              className="absolute top-[-2.5rem] left-2/4 z-30 -translate-x-2/4 scale-50 sm:top-[1.5rem] sm:-right-[1.5rem] sm:translate-0 sm:scale-75 md:top-[6rem] md:scale-100"
+              className="absolute top-[-2.5rem] left-2/4 z-30 -translate-x-2/4 scale-50 sm:top-[1.5rem] sm:-right-[1.5rem] sm:left-[unset] sm:translate-0 sm:scale-75 md:top-[6rem] md:scale-100"
               style={{
                 transform: `translateY(${parallax / 30}px)`,
               }}
